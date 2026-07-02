36101f1ab703e0f0e7ba26c56c6158f1d9d3f794
vamshi <kanavenavamshi1414@gmail.com>
2026-07-02 20:11:05 +0530

    same line confliect

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 @app.route('/api/books', methods=['GET'])
 def get_books():
     books = [
-        {"id": 1, "title": "Python Basics", "author": "John Doe"},
+        {"id": 1, "title": "Python Basics", "author": "Lin Doe"},
         {"id": 2, "title": "Flask for Beginners", "author": "Jane Smith"}
     ]
     return jsonify(books)
@@ -37,4 +37,4 @@ def add_book():
     }), 201
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=9000)
